6091c92e24ea1cc2ffb81773a6082f9e4876bd03
Kingswibu <99636581+Kingswibu@users.noreply.github.com>
2022-02-17 02:15:43 +0700

    Update logomaker.py

--- PrimeMega/modules/logomaker.py
+++ PrimeMega/modules/logomaker.py
@@ -113,7 +113,7 @@ async def logo_gen(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [ʜɪʀᴏsʜɪ ʀᴏʙᴏᴛ](https://t.me/HiroshiMusicBot)",
+            caption="Logo by [ʜᴜɴᴛᴇʀ ʀᴏʙᴏᴛ](https://t.me/Hunter_v_bot)",
             force_document=False,
         )
         os.remove(flnme)
@@ -196,7 +196,7 @@ async def logo_(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [ʜɪʀᴏsʜɪ ʀᴏʙᴏᴛ](https://t.me/HiroshiMusicBot)",
+            caption="Logo by [ʜᴜɴᴛᴇʀ ʀᴏʙᴏᴛ](https://t.me/Hunter_v_bot)",
             force_document=False,
         )
         os.remove(flnme)
